6873c4d816d342bbfa6e38b532986dc495035206
Paweu@Hydra <pgrabas@gmail.com>
2016-04-10 12:49:13 +0200

    implemnted lib clonning in build system

--- Build/Libraries.lua
+++ Build/Libraries.lua
@@ -1,30 +1,67 @@
 
-local function CloneLibrary(conf, libconf)
-	print "Lib clonning is not implemeted"
-	os.exit(1)
+local function execute(cmd)
+--	print(cmd)
+	io.flush(io.output())
+	local file = io.popen(cmd)
+	local l
+	local ret = { }
+	while true do 
+		l = file:read "*l"
+		if not l then
+			break
+		end
+		ret[#ret + 1] = l
+	end
+	file:close()
+	return ret
 end
 
-local function CheckLibraryStatus(conf, libconf)
-	os.chdir(libconf.path)
-	
-	local result = { os.execute("git diff-index --quiet HEAD --") }
-	
-	local changed = result[#result]
+local function firstline(cmd)
+	local r = execute(cmd)
+	return r[1]
+end
 
-	if changed == 1 then
-		print(string.format("Library %s is CHANGED", libconf.name))
-	end
+
+local function CloneLibrary(conf, libconf)
+	os.chdir(conf.path)
+	execute(string.format("git clone -b %s %s", libconf.branch, libconf.remotes["origin"]))
+	
+--	print "Lib clonning is not implemeted"
+	--os.exit(1)
 end
 
 local function ProcessLibrary(conf, libconf)
-	--print("Processing lib: ".. libconf.name .. "   " .. conf.startpath)
+	io.write(string.format("Checking lib: %-15s ", libconf.name .. "..."))
 	libconf.path = conf.path .. libconf.name
 
-	if not os.isdir(libconf.path) then
-		return CloneLibrary(conf, libconf)
+	libconf.exists = os.isdir(libconf.path)
+
+	if not libconf.exists then
+		if libconf.cloneattempt then
+			print "Clone attempt failed."
+			print "Cannot prepare build system."
+			os.exit(1)
+		end
+	
+		libconf.cloneattempt = true
+		print("Not exists. Clonning...")
+		CloneLibrary(conf, libconf)
+		return ProcessLibrary(conf, libconf)
 	end
 	
-	return CheckLibraryStatus(conf, libconf)
+	os.chdir(libconf.path)
+	
+	local branch = firstline("git rev-parse --symbolic-full-name --abbrev-ref HEAD")
+	local commithash = firstline("git rev-parse HEAD")
+	
+	print (string.format("Status: %s, branch: %s, commit:%s", "ok", branch, commithash))
+	
+	--local result = { os.execute("git diff --cached --exit-code") }
+	--local changed = result[#result]
+--	if changed == 1 then
+--		print(string.format("Library %s is CHANGED", libconf.name))
+--	end
+
 end
 
 function Libraries(config) 
@@ -33,7 +70,7 @@ function Libraries(config)
 	end
 	
 	local conf = {
-		path = config.path,
+		path = dir.libsrc,
 		startpath = os.getcwd() .. "/",
 	}
 	
@@ -46,7 +83,5 @@ function Libraries(config)
 end
 
 function MoonGlare.CheckLibraries()
-
-	include(dir.libsrc .. "LibrariesConfig.lua")
-	
+	include(dir.libsrc .. "LibConfig.lua")
 end
---
+++ Libs/LibConfig.lua
@@ -0,0 +1,26 @@
+
+Libraries{
+	libs = {
+		{ 
+			name = "OrbitLogger",
+			remotes = { 
+				origin = "git@smaug.lan:~/projects/libs/OrbitLogger.git",
+			},
+			branch = "master",
+		},
+		{ 
+			name = "StarVFS",
+			remotes = { 
+				origin = "git@smaug.lan:~/projects/libs/StarVFS.git",
+			},
+			branch = "master",
+		},		
+		{ 
+			name = "libSpace",
+			remotes = { 
+				origin = "git@smaug.lan:~/projects/libs/libSpace.git",
+			},
+			branch = "master",
+		},			
+	},
+}
