e5a1660f3c4a4f27bedf87dff59cd6a4d9538bed
Paweu@Hydra <pgrabas@gmail.com>
2016-09-30 17:19:36 +0200

    [GUIComponentUpdate] reversed file which shall not be commited

--- Build/CustomRules.lua
+++ Build/CustomRules.lua
@@ -4,30 +4,21 @@
 --	display "xml2cpp"
 --	fileExtension ".x2c"
 
-MoonGlare.X2C = { }
-
 function MoonGlare.X2CRule()
 
 --filter 'files:**.x2c'
 --	buildmessage 'Processing %{file.relpath} with xml2cpp'
 --	buildcommands 'xml2cpp --input "%{file.relpath}" --output "%{cfg.objdir}%{file.basename}.x2c.h"'
 --	buildoutputs '%{cfg.objdir}%{file.basename}.x2c.h"'
-
-	filter 'files:**/Shared/x2c/**.x2c'
-		buildmessage 'Processing %{file.name} with xml2cpp'
-		buildcommands 'xml2cpp --input "%{file.relpath}" --output "%{cfg.objdir}/%{file.basename}.x2c.h"'
-		buildoutputs '%{cfg.objdir}/%{file.basename}.x2c.h'
-
-
+  
+	filter 'files:**/Shared/x2c/**.x2c'		
+		buildmessage 'Processing %{file.name} with xml2cpp'		
+		buildcommands 'xml2cpp --input "%{file.relpath}" --output "%{cfg.objdir}%{file.basename}.x2c.h"'		
+		buildoutputs '%{cfg.objdir}%{file.basename}.x2c.h'		  
+  
+  
 	filter {}
 end
-
-function MoonGlare.X2C.Files(list)
-    for i,v in ipairs(list) do
-        print(v)
-    end
-end
-
 --[[
 
 rule "VersionCompiler"
@@ -37,5 +28,5 @@ rule "VersionCompiler"
 	 buildmessage 'Compiling %(Filename) with MyCustomCC'
 	 buildcommand 'verupdate --input=%(FullPath) --output=.\\ %(ProjectName)'
 	 buildoutputs '%(ProjectName)Version.inc"'
-
+	
 ]]
