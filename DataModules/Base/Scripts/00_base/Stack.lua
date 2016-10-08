750881bc177dd6c2c0edbe05b5efcbf975fb6354
Paweu@Hydra <pgrabas@gmail.com>
2016-10-08 18:39:46 +0200

    [Fixes] stack in lua improvement

--- DataModules/Base/Scripts/00_base/Stack.lua
+++ DataModules/Base/Scripts/00_base/Stack.lua
@@ -1,19 +1,29 @@
 -- simple implementation of stack
-
 Stack = {}
- 
+
+local StackIndex = { }
+local StackMT = {
+    __index = StackIndex,
+--    __newindex = function()
+--        error("Attempt to modify stack")
+--        return nil
+--    end,
+}
+
+function StackIndex:push(item)
+  self[#self + 1] = item
+end
+
+function StackIndex:pop()
+  local item = self[#self]
+  self[#self] = nil
+  return item
+end
+
+function StackIndex:top()
+  return self[#self]
+end
+
 function Stack:Create()
-  local stack = {}
- 
-  function stack:push(item)
-    self[#self + 1] = item
-  end
- 
-  function stack:pop()
-    local item = self[#self]
-    self[#self] = nil
-    return item
-  end
- 
-  return stack
+  return setmetatable({}, StackMT)
 end
