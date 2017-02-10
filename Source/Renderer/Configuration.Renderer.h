2c0594755f14532988632c477fb1fa2835e16527
Kalessin <pgrabas@gmail.com>
2017-02-10 20:18:10 +0100

    [Renderer] basic classes

---
+++ Source/Renderer/Configuration.Renderer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace MoonGlare::Renderer::Configuration {
+
+static constexpr size_t FrameBufferSize = 10 * 1024 * 1024;
+
+static constexpr size_t FramesCount = 3;
+
+} //namespace MoonGlare::Renderer::Configuration
