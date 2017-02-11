6cd8ce7ab89cc1f45a5780546271c36043644e79
Kalessin <pgrabas@gmail.com>
2017-02-11 09:36:56 +0100

    [Renderer] Early BucketCommandQueue

--- Source/Renderer/Configuration.Renderer.h
+++ Source/Renderer/Configuration.Renderer.h
@@ -2,8 +2,19 @@
 
 namespace MoonGlare::Renderer::Configuration {
 
-static constexpr size_t FrameBufferSize = 10 * 1024 * 1024;
+struct FrameBuffer {
+	static constexpr uint32_t MemorySize = 1 * 1024 * 1024;
+	static constexpr uint32_t Count = 3;
+};
 
-static constexpr size_t FramesCount = 3;
+struct CommandBucket {
+	static constexpr uint32_t MaxCommandsPerBucket = 16;
+	using BucketCommandCount = uint8_t;
+	using BucketCommandIndex = uint16_t;
+	static constexpr uint32_t MaxBuckets = 1024;
+	static constexpr uint32_t MaxCommands = 4096;
+
+	static constexpr uint32_t ArgumentMemoryBuffer = 512 * 1024;
+};
 
 } //namespace MoonGlare::Renderer::Configuration
