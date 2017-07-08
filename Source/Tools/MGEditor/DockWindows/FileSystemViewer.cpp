6c920d491d707e8aafd3917ef94eeace9c32a612
Kalessin <pgrabas@gmail.com>
2017-07-08 09:37:05 +0200

    [master] editor update

--- Source/Tools/MGEditor/DockWindows/FileSystemViewer.cpp
+++ Source/Tools/MGEditor/DockWindows/FileSystemViewer.cpp
@@ -22,246 +22,246 @@ namespace Editor {
 namespace DockWindows {
 
 struct FileSystemViewerInfo 
-	: public QtShared::DockWindowModule<FileSystemViewer>
-	, public QtShared::iEditorInfo {
+    : public QtShared::DockWindowModule<FileSystemViewer>
+    , public QtShared::iEditorInfo {
 
-	FileSystemViewerInfo(SharedModuleManager modmgr) : DockWindowModule(std::move(modmgr)) {
-		SetSettingID("FileSystemViewerInfo");
-		SetDisplayName(tr("Filesystem"));
-		SetShortcut("F4");
-	}
+    FileSystemViewerInfo(SharedModuleManager modmgr) : DockWindowModule(std::move(modmgr)) {
+        SetSettingID("FileSystemViewerInfo");
+        SetDisplayName(tr("Filesystem"));
+        SetShortcut("F4");
+    }
 
-	virtual std::vector<FileHandleMethodInfo> GetCreateFileMethods() const override {
-		return std::vector<FileHandleMethodInfo> {
-			FileHandleMethodInfo{ "{DIR}", ICON_16_FOLDER_RESOURCE, "Folder", "{DIR}", },
-		};
-	}
+    virtual std::vector<FileHandleMethodInfo> GetCreateFileMethods() const override {
+        return std::vector<FileHandleMethodInfo> {
+            FileHandleMethodInfo{ "{DIR}", ICON_16_FOLDER_RESOURCE, "Folder", "{DIR}", },
+        };
+    }
 };
 QtShared::ModuleClassRgister::Register<FileSystemViewerInfo> FileSystemViewerInfoReg("FileSystemViewer");
 
 //-----------------------------------------
 
 FileSystemViewer::FileSystemViewer(QWidget * parent, QtShared::WeakModule module)
-	:  QtShared::DockWindow(parent) {
+    :  QtShared::DockWindow(parent) {
 
-	m_EditorModule = module;
-	auto mm = m_EditorModule.lock()->GetModuleManager();
-	m_FileIconProvider = mm->QuerryModule<QtShared::FileIconProvider>();
-	m_EditorProvider = mm->QuerryModule<QtShared::EditorProvider>();
+    m_EditorModule = module;
+    auto mm = m_EditorModule.lock()->GetModuleManager();
+    m_FileIconProvider = mm->QuerryModule<QtShared::FileIconProvider>();
+    m_EditorProvider = mm->QuerryModule<QtShared::EditorProvider>();
 
-	SetSettingID("FileSystemViewer");
-	m_Ui = std::make_unique<Ui::FilesystemViewer>();
-	m_Ui->setupUi(this);
+    SetSettingID("FileSystemViewer");
+    m_Ui = std::make_unique<Ui::FilesystemViewer>();
+    m_Ui->setupUi(this);
 
-	m_FileSystem = MainWindow::Get()->GetFilesystem();
+    m_FileSystem = MainWindow::Get()->GetFilesystem();
 
-	m_Ui->treeView->setContextMenuPolicy(Qt::CustomContextMenu);
-	connect(m_Ui->treeView, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(ShowContextMenu(const QPoint &)));
-	connect(m_Ui->treeView, SIGNAL(doubleClicked(const QModelIndex&)), SLOT(ItemDoubleClicked(const QModelIndex&)));
+    m_Ui->treeView->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(m_Ui->treeView, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(ShowContextMenu(const QPoint &)));
+    connect(m_Ui->treeView, SIGNAL(doubleClicked(const QModelIndex&)), SLOT(ItemDoubleClicked(const QModelIndex&)));
 
-	connect(Notifications::Get(), SIGNAL(ProjectChanged(Module::SharedDataModule)), this, SLOT(ProjectChanged(Module::SharedDataModule)));
-	connect(m_FileSystem.get(), SIGNAL(Changed()), this, SLOT(RefreshFilesystem()));
+    connect(Notifications::Get(), SIGNAL(ProjectChanged(Module::SharedDataModule)), this, SLOT(ProjectChanged(Module::SharedDataModule)));
+    connect(m_FileSystem.get(), SIGNAL(Changed()), this, SLOT(RefreshFilesystem()));
 
-	m_ViewModel = std::make_unique<QStandardItemModel>();
-	m_ViewModel->setSortRole(FileSystemViewerRole::SortString);
+    m_ViewModel = std::make_unique<QStandardItemModel>();
+    m_ViewModel->setSortRole(FileSystemViewerRole::SortString);
 
-	m_Ui->treeView->setModel(m_ViewModel.get());
-	m_Ui->treeView->setSelectionMode(QAbstractItemView::SingleSelection);
-	m_Ui->treeView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    m_Ui->treeView->setModel(m_ViewModel.get());
+    m_Ui->treeView->setSelectionMode(QAbstractItemView::SingleSelection);
+    m_Ui->treeView->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
-	m_Ui->treeView->setColumnWidth(0, 200);
+    m_Ui->treeView->setColumnWidth(0, 200);
 }
 
 FileSystemViewer::~FileSystemViewer() {
-	m_EditorProvider.reset();
-	m_FileIconProvider.reset();
-	m_Ui.reset();
+    m_EditorProvider.reset();
+    m_FileIconProvider.reset();
+    m_Ui.reset();
 }
 
 bool FileSystemViewer::DoSaveSettings(pugi::xml_node node) const {
-	QtShared::DockWindow::DoSaveSettings(node);
-	return true;
+    QtShared::DockWindow::DoSaveSettings(node);
+    return true;
 }
 
 bool FileSystemViewer::DoLoadSettings(const pugi::xml_node node) {
-	QtShared::DockWindow::DoLoadSettings(node);
-	return true;
+    QtShared::DockWindow::DoLoadSettings(node);
+    return true;
 }
 
 bool FileSystemViewer::Create(const std::string & LocationURI, const QtShared::iEditorInfo::FileHandleMethodInfo & what) {
-	QString qname;
-	if (!QuerryStringInput("Enter name:", qname))
-		return false;
+    QString qname;
+    if (!QuerryStringInput("Enter name:", qname))
+        return false;
 
-	std::string name = qname.toLocal8Bit().constData();
-	std::string URI = LocationURI + name;
+    std::string name = qname.toLocal8Bit().constData();
+    std::string URI = LocationURI + name;
 
-	return m_FileSystem->CreateDirectory(URI);
+    return m_FileSystem->CreateDirectory(URI);
 }
 
 void FileSystemViewer::ShowContextMenu(const QPoint &point) {
-	QMenu menu;
-	QModelIndex index = m_Ui->treeView->indexAt(point);
-	auto itemptr = m_ViewModel->itemFromIndex(index);
+    QMenu menu;
+    QModelIndex index = m_Ui->treeView->indexAt(point);
+    auto itemptr = m_ViewModel->itemFromIndex(index);
 
-	bool folder = !index.data(FileSystemViewerRole::IsFile).toBool();
-	if (folder) {
-		auto *CreateMenu = menu.addMenu(ICON_16_CREATE_RESOURCE, "Create");
-		for (auto methodmodule : m_EditorProvider.lock()->GetCreateMethods()) {
-			if (!methodmodule.m_EditorFactory)
-				continue;
+    bool folder = !index.data(FileSystemViewerRole::IsFile).toBool();
+    if (folder) {
+        auto *CreateMenu = menu.addMenu(ICON_16_CREATE_RESOURCE, "Create");
+        for (auto methodmodule : m_EditorProvider.lock()->GetCreateMethods()) {
+            if (!methodmodule.m_EditorFactory)
+                continue;
 
-			auto &method = methodmodule.m_FileHandleMethod;
+            auto &method = methodmodule.m_FileHandleMethod;
 
-			CreateMenu->addAction(QIcon(method.m_Icon.c_str()), method.m_Caption.c_str(), [this, methodmodule, index]() {
-				auto qstr = index.data(FileSystemViewerRole::FileURI).toString();
-				std::string URI = qstr.toLocal8Bit().constData();
+            CreateMenu->addAction(QIcon(method.m_Icon.c_str()), method.m_Caption.c_str(), [this, methodmodule, index]() {
+                auto qstr = index.data(FileSystemViewerRole::FileURI).toString();
+                std::string URI = qstr.toLocal8Bit().constData();
 
-				QtShared::iEditorFactory::EditorRequestOptions ero;
-				auto editor = methodmodule.m_EditorFactory->GetEditor(methodmodule.m_FileHandleMethod, ero);
-				if (!editor) {
-					AddLogf(Error, "Failure during editor creation!");
-					ErrorMessage("Cannot create editor!");
-					return;
-				}
+                QtShared::iEditorFactory::EditorRequestOptions ero;
+                auto editor = methodmodule.m_EditorFactory->GetEditor(methodmodule.m_FileHandleMethod, ero);
+                if (!editor) {
+                    AddLogf(Error, "Failure during editor creation!");
+                    ErrorMessage("Cannot create editor!");
+                    return;
+                }
 
-				if (!editor->Create(URI, methodmodule.m_FileHandleMethod)) {
-					AddLogf(Error, "Failure during creation!");
-					ErrorMessage("Cannot create reqesterd file!");
-				}
-			});
-		}
+                if (!editor->Create(URI, methodmodule.m_FileHandleMethod)) {
+                    AddLogf(Error, "Failure during creation!");
+                    ErrorMessage("Cannot create reqesterd file!");
+                }
+            });
+        }
 
-	} else {
-		menu.addAction("Open", this, &FileSystemViewer::OpenItem);
-	}
+    } else {
+        menu.addAction("Open", this, &FileSystemViewer::OpenItem);
+    }
 
-	menu.addSeparator();
+    menu.addSeparator();
 
-	if (itemptr) {
-		menu.addAction("Copy URI", [itemptr]() {
-			auto qstr = itemptr->data(FileSystemViewerRole::FileURI).toString();
-			QClipboard *clipboard = QApplication::clipboard();
-			clipboard->setText(qstr);
-		});
-		menu.addAction("Copy hash URI", [itemptr]() {
-			auto qstr = itemptr->data(FileSystemViewerRole::FileHash).toString();
-			QClipboard *clipboard = QApplication::clipboard();
-			clipboard->setText(qstr);
-		});
-		menu.addSeparator();
-	}
+    if (itemptr) {
+        menu.addAction("Copy URI", [itemptr]() {
+            auto qstr = itemptr->data(FileSystemViewerRole::FileURI).toString();
+            QClipboard *clipboard = QApplication::clipboard();
+            clipboard->setText(qstr);
+        });
+        menu.addAction("Copy hash URI", [itemptr]() {
+            auto qstr = itemptr->data(FileSystemViewerRole::FileHash).toString();
+            QClipboard *clipboard = QApplication::clipboard();
+            clipboard->setText(qstr);
+        });
+        menu.addSeparator();
+    }
 
-	menu.addAction(ICON_16_REFRESH, "Refresh", [this]() {
-		m_FileSystem->Reload();
-		RefreshFilesystem();
-	});
+    menu.addAction(ICON_16_REFRESH, "Refresh", [this]() {
+        m_FileSystem->Reload();
+        RefreshFilesystem();
+    });
 
-	menu.exec(m_Ui->treeView->mapToGlobal(point));
+    menu.exec(m_Ui->treeView->mapToGlobal(point));
 }
 
 void FileSystemViewer::ItemDoubleClicked(const QModelIndex&) {
-	OpenItem();
+    OpenItem();
 }
 
 void FileSystemViewer::OpenItem() {
-	auto selection = m_Ui->treeView->selectionModel()->currentIndex();
-	auto parent = selection.parent();
+    auto selection = m_Ui->treeView->selectionModel()->currentIndex();
+    auto parent = selection.parent();
 
-	auto itemptr = m_ViewModel->itemFromIndex(selection);
-	if (!itemptr)
-		return;
+    auto itemptr = m_ViewModel->itemFromIndex(selection);
+    if (!itemptr)
+        return;
 
-	std::string fullpath = itemptr->data(FileSystemViewerRole::FileURI).toString().toLocal8Bit().constData();
+    std::string fullpath = itemptr->data(FileSystemViewerRole::FileURI).toString().toLocal8Bit().constData();
     MainWindow::Get()->OpenFileEditor(fullpath);
 }
 
 void FileSystemViewer::RefreshFilesystem() {
-	if (!m_Module) {
-	}
-	//m_FileSystem->Reload();
-	RefreshTreeView();
+    if (!m_Module) {
+    }
+    //m_FileSystem->Reload();
+    RefreshTreeView();
 }
 
 void FileSystemViewer::ProjectChanged(Module::SharedDataModule datamod) {
-	m_Module = datamod;
+    m_Module = datamod;
 }
 
 void FileSystemViewer::Clear() {
 }
 
 void FileSystemViewer::RefreshTreeView() {
-	m_ViewModel->clear();
-	m_ViewModel->setHorizontalHeaderItem(0, new QStandardItem("File"));
+    m_ViewModel->clear();
+    m_ViewModel->setHorizontalHeaderItem(0, new QStandardItem("File"));
 
-	decltype(m_VFSItemMap) currmap;
-	auto svfs = m_FileSystem->GetVFS();
+    decltype(m_VFSItemMap) currmap;
+    auto svfs = m_FileSystem->GetVFS();
 
-	StarVFS::HandleEnumerateFunc processitem;
-	processitem = [&](StarVFS::FileID fid) -> bool {
-		QStandardItem *parent = nullptr;
-		QStandardItem *item = nullptr;
-		std::swap(m_VFSItemMap[fid], item);
+    StarVFS::HandleEnumerateFunc processitem;
+    processitem = [&](StarVFS::FileID fid) -> bool {
+        QStandardItem *parent = nullptr;
+        QStandardItem *item = nullptr;
+        std::swap(m_VFSItemMap[fid], item);
 
-		auto h = svfs->OpenFile(fid);
-		if (!h)
-			return true;
+        auto h = svfs->OpenFile(fid);
+        if (!h)
+            return true;
 
-		if (!item) {
-			auto pid = h.GetParrentID();
-			parent = currmap[pid];
-			if (!parent)
-				parent = m_ViewModel->invisibleRootItem();
+        if (!item) {
+            auto pid = h.GetParrentID();
+            parent = currmap[pid];
+            if (!parent)
+                parent = m_ViewModel->invisibleRootItem();
 
-			QList<QStandardItem*> cols;
-			cols << (item = new QStandardItem(h.GetName()));
-			parent->appendRow(cols);
-		}
-		currmap[fid] = item;
+            QList<QStandardItem*> cols;
+            cols << (item = new QStandardItem(h.GetName()));
+            parent->appendRow(cols);
+        }
+        currmap[fid] = item;
 
-		if (h.IsDirectory()) {
-			item->setData(ICON_16_FOLDER, Qt::DecorationRole);
-			h.EnumerateChildren(processitem);
-			item->setData(QVariant(false), FileSystemViewerRole::IsFile);
-		} else {
-			item->setData(QVariant(true), FileSystemViewerRole::IsFile);
-			auto ext = strrchr(h.GetName(), '.');
-			if (ext) {
-				++ext;
-				item->setData(QIcon(m_FileIconProvider->GetExtensionIcon(ext, "").c_str()), Qt::DecorationRole);
-			} else
-				item->setData(QIcon(), Qt::DecorationRole);
-		}
+        if (h.IsDirectory()) {
+            item->setData(ICON_16_FOLDER, Qt::DecorationRole);
+            h.EnumerateChildren(processitem);
+            item->setData(QVariant(false), FileSystemViewerRole::IsFile);
+        } else {
+            item->setData(QVariant(true), FileSystemViewerRole::IsFile);
+            auto ext = strrchr(h.GetName(), '.');
+            if (ext) {
+                ++ext;
+                item->setData(QIcon(m_FileIconProvider->GetExtensionIcon(ext, "").c_str()), Qt::DecorationRole);
+            } else
+                item->setData(QIcon(), Qt::DecorationRole);
+        }
 
-		auto str = h.GetFullPath();
-		if (h.IsDirectory() && str.back() != '/')
-			str.push_back('/');
-		auto hashuri = StarVFS::MakePathHashURI(h.GetHash());
-		auto fileuri = StarVFS::MakeFileURI(str.c_str());
-		item->setData(fileuri.c_str(), FileSystemViewerRole::FileURI);
-		item->setData(hashuri.c_str(), FileSystemViewerRole::FileHash);
-		item->setData(str.c_str(), FileSystemViewerRole::FileFullName);
+        auto str = h.GetFullPath();
+        if (h.IsDirectory() && str.back() != '/')
+            str.push_back('/');
+        auto hashuri = StarVFS::MakePathHashURI(h.GetHash());
+        auto fileuri = StarVFS::MakeFileURI(str.c_str());
+        item->setData(fileuri.c_str(), FileSystemViewerRole::FileURI);
+        item->setData(hashuri.c_str(), FileSystemViewerRole::FileHash);
+        item->setData(str.c_str(), FileSystemViewerRole::FileFullName);
 
-		std::string sortstring;
-		sortstring = h.IsDirectory() ? "D" : "F";
-		sortstring += h.GetName();
-		item->setData(sortstring.c_str(), FileSystemViewerRole::SortString);
+        std::string sortstring;
+        sortstring = h.IsDirectory() ? "D" : "F";
+        sortstring += h.GetName();
+        item->setData(sortstring.c_str(), FileSystemViewerRole::SortString);
 
-		h.Close();
-		
-		return true;
-	};
+        h.Close();
+        
+        return true;
+    };
 
-	currmap.swap(m_VFSItemMap);
-	for (auto &it : currmap)
-		delete it.second;
+    currmap.swap(m_VFSItemMap);
+    for (auto &it : currmap)
+        delete it.second;
 
-	auto root = svfs->OpenFile("/");
-	//processitem(root.GetFID());
-	root.EnumerateChildren(processitem);
-	root.Close();
+    auto root = svfs->OpenFile("/");
+    //processitem(root.GetFID());
+    root.EnumerateChildren(processitem);
+    root.Close();
 
-	m_ViewModel->sort(0);
+    m_ViewModel->sort(0);
 }
 
 } //namespace DockWindows 
--- Source/Tools/MGEditor/Editors/EntityEditor.cpp
+++ Source/Tools/MGEditor/Editors/EntityEditor.cpp
@@ -24,210 +24,222 @@ namespace Editor {
 namespace EntityEditor {
 
 struct EntityEditorModule
-	: public QtShared::BaseDockWindowModule
-	, public QtShared::iFileIconInfo 
-	, public QtShared::iEditorInfo {
+    : public QtShared::BaseDockWindowModule
+    , public QtShared::iFileIconInfo 
+    , public QtShared::iEditorInfo 
+    , public QtShared::iEditorFactory
+{
 
-	EntityEditorModule(SharedModuleManager modmgr) : BaseDockWindowModule(std::move(modmgr)) {
-		SetSettingID("EntityEditorInfo");
-		SetDisplayName(tr("EntityEditor"));
-		SetShortcut("F2");
-	}
+    EntityEditorModule(SharedModuleManager modmgr) : BaseDockWindowModule(std::move(modmgr)) {
+        SetSettingID("EntityEditorInfo");
+        SetDisplayName(tr("EntityEditor"));
+        SetShortcut("F2");
+    }
 
-	virtual std::shared_ptr<QtShared::DockWindow> CreateInstance(QWidget *parent) override {
-		return std::make_shared<EntityEditorWindow>(parent, GetModuleManager());
-	}
+    std::shared_ptr<EntityEditorWindow> window;
 
-	std::vector<FileIconInfo> GetFileIconInfo() const override {
-		return std::vector<FileIconInfo>{
-			FileIconInfo{ "epx", ICON_16_ENTITYPATTERN_RESOURCE, },
-		};
-	}		
+    virtual std::shared_ptr<QtShared::DockWindow> CreateInstance(QWidget *parent) override {
+        if(!window)
+            window = std::make_shared<EntityEditorWindow>(parent, GetModuleManager());
+        return window;
+    }
 
-	virtual std::vector<FileHandleMethodInfo> GetCreateFileMethods() const override {
-		return std::vector<FileHandleMethodInfo> {
-			FileHandleMethodInfo{ "epx", ICON_16_ENTITYPATTERN_RESOURCE, "Entity pattern...", "create", },
-		};
-	}
+    std::vector<FileIconInfo> GetFileIconInfo() const override {
+        return std::vector<FileIconInfo>{
+            FileIconInfo{ "epx", ICON_16_ENTITYPATTERN_RESOURCE, },
+        };
+    }		
 
-	virtual std::vector<FileHandleMethodInfo> GetOpenFileMethods() const override {
-		return std::vector<FileHandleMethodInfo> {
-			FileHandleMethodInfo{ "epx", ICON_16_ENTITYPATTERN_RESOURCE, "Edit entity pattern", "open", },
-		};
-	}
+    virtual std::vector<FileHandleMethodInfo> GetCreateFileMethods() const override {
+        return std::vector<FileHandleMethodInfo> {
+            FileHandleMethodInfo{ "epx", ICON_16_ENTITYPATTERN_RESOURCE, "Entity pattern...", "create", },
+        };
+    }
+
+    virtual std::vector<FileHandleMethodInfo> GetOpenFileMethods() const override {
+        return std::vector<FileHandleMethodInfo> {
+            FileHandleMethodInfo{ "epx", ICON_16_ENTITYPATTERN_RESOURCE, "Edit entity pattern", "open", },
+        };
+    }
+
+    QtShared::SharedEditor GetEditor(const iEditorInfo::FileHandleMethodInfo &method, const EditorRequestOptions&options) const override {
+       // if (!window)
+       //     window = std::make_shared<EntityEditorWindow>(parent, GetModuleManager());
+        return window;
+    }
 };
 QtShared::ModuleClassRgister::Register<EntityEditorModule> EntityEditorReg("EntityEditor");
 
 //----------------------------------------------------------------------------------
 
 EntityEditorWindow::EntityEditorWindow(QWidget * parent, QtShared::SharedModuleManager modmgr)
-	:  QtShared::DockWindow(parent) {
+    :  QtShared::DockWindow(parent) {
     moduleManager.swap(modmgr);
     m_Ui = std::make_unique<Ui::EntityEditor>();
-	m_Ui->setupUi(this);
+    m_Ui->setupUi(this);
 
-	SetSettingID("EntityEditorWindow");
-	m_Ui->EntityTree->SetSettingID("EntityTree");
-	SetChangesName("EntityEditor");
-	InsertChangesChild(m_Ui->EntityTree, "Entities");
+    SetSettingID("EntityEditorWindow");
+    m_Ui->EntityTree->SetSettingID("EntityTree");
+    SetChangesName("EntityEditor");
+    InsertChangesChild(m_Ui->EntityTree, "Entities");
 
-	connect(Notifications::Get(), SIGNAL(RefreshView()), SLOT(Refresh()));
+    connect(Notifications::Get(), SIGNAL(RefreshView()), SLOT(Refresh()));
 
     m_Ui->EntityTree->SetModuleManager(moduleManager);
-	auto ue = std::make_unique<BaseEntity>();
-	m_BaseEntity = ue.get();
-	m_Ui->EntityTree->SetEntity(std::move(ue));
+    auto ue = std::make_unique<BaseEntity>();
+    m_BaseEntity = ue.get();
+    m_Ui->EntityTree->SetEntity(std::move(ue));
 }
 
 EntityEditorWindow::~EntityEditorWindow() {
-	m_Ui.reset();
+    m_Ui.reset();
 }
 
 //----------------------------------------------------------------------------------
 
 bool EntityEditorWindow::DoSaveSettings(pugi::xml_node node) const {
-	QtShared::DockWindow::DoSaveSettings(node);
-	SaveChildSettings(node, m_Ui->EntityTree);
-	return true;
+    QtShared::DockWindow::DoSaveSettings(node);
+    SaveChildSettings(node, m_Ui->EntityTree);
+    return true;
 }
 
 bool EntityEditorWindow::DoLoadSettings(const pugi::xml_node node) {
-	QtShared::DockWindow::DoLoadSettings(node);
-	LoadChildSettings(node, m_Ui->EntityTree);
-	return true;
+    QtShared::DockWindow::DoLoadSettings(node);
+    LoadChildSettings(node, m_Ui->EntityTree);
+    return true;
 }
 
 //----------------------------------------------------------------------------------
 
 void EntityEditorWindow::Refresh() {
-	m_Ui->EntityTree->Refresh();
+    m_Ui->EntityTree->Refresh();
 }
 
 void EntityEditorWindow::Clear() {
-	m_BaseEntity->Clear();
+    m_BaseEntity->Clear();
 }
 
 //----------------------------------------------------------------------------------
 
 bool EntityEditorWindow::Create(const std::string &LocationURI, const QtShared::iEditorInfo::FileHandleMethodInfo& what) {
-	QString qname;
-	if (!QuerryStringInput("Enter name:", qname))
-		return false;
+    QString qname;
+    if (!QuerryStringInput("Enter name:", qname, 2))
+        return false;
 
-	std::string name = qname.toLocal8Bit().constData();
-	std::string URI = LocationURI + name + ".epx";
+    std::string name = qname.toLocal8Bit().constData();
+    std::string URI = LocationURI + name + ".epx";
 
-	auto fs = MainWindow::Get()->GetFilesystem();
-	if (!fs->CreateFile(URI)) {
-		ErrorMessage("Failed during creating epx file");
-		AddLog(Hint, "Failed to create epx: " << m_CurrentPatternFile);
-		return false;
-	}
+    auto fs = MainWindow::Get()->GetFilesystem();
+    if (!fs->CreateFile(URI)) {
+        ErrorMessage("Failed during creating epx file");
+        AddLog(Hint, "Failed to create epx: " << m_CurrentPatternFile);
+        return false;
+    }
 
-	m_BaseEntity->GetName() = name;
+    m_BaseEntity->GetName() = name;
 
-	m_CurrentPatternFile = URI;
-	SetModiffiedState(true);
-	Refresh();
+    m_CurrentPatternFile = URI;
+    SetModiffiedState(true);
+    Refresh();
 
-	AddLog(Hint, "Created epx file: " << URI);
+    AddLog(Hint, "Created epx file: " << URI);
 
-	if (!SaveData()) {
-		ErrorMessage("Failed during saving epx file");
-		AddLog(Hint, "Failed to save epx: " << m_CurrentPatternFile);
-	} else {
-		SetModiffiedState(false);
-	}
+    if (!SaveData()) {
+        ErrorMessage("Failed during saving epx file");
+        AddLog(Hint, "Failed to save epx: " << m_CurrentPatternFile);
+    } else {
+        SetModiffiedState(false);
+    }
 
-	return true;
+    return true;
 }
 
 bool EntityEditorWindow::TryCloseData() {
-	AddLog(Hint, "Trying to close epx: " << m_CurrentPatternFile);
-	if (m_BaseEntity && IsChanged()) {
-		if (!AskForPermission("There is a opened pattern. Do you want to close it?"))
-			return false;
-		if (AskForPermission("Save changes?"))
-			SaveData();
-	}
-	m_CurrentPatternFile.clear();
-	Clear();
-	Refresh();
-	return true;
+    AddLog(Hint, "Trying to close epx: " << m_CurrentPatternFile);
+    if (m_BaseEntity && IsChanged()) {
+        if (!AskForPermission("There is a opened pattern. Do you want to close it?"))
+            return false;
+        if (AskForPermission("Save changes?"))
+            SaveData();
+    }
+    m_CurrentPatternFile.clear();
+    Clear();
+    Refresh();
+    return true;
 }
 
 bool EntityEditorWindow::OpenData(const std::string &file) {
-	if (!TryCloseData())
-		return false;
+    if (!TryCloseData())
+        return false;
 
-	try {
-		pugi::xml_document xdoc;
+    try {
+        pugi::xml_document xdoc;
 
-		auto fs = MainWindow::Get()->GetFilesystem();
-		StarVFS::ByteTable bt;
-		if (!fs->GetFileData(file, bt)) {
-			//todo: log sth
-			throw false;
-		}
-		if (bt.byte_size() == 0) {
-			throw true;
-		}
+        auto fs = MainWindow::Get()->GetFilesystem();
+        StarVFS::ByteTable bt;
+        if (!fs->GetFileData(file, bt)) {
+            //todo: log sth
+            throw false;
+        }
+        if (bt.byte_size() == 0) {
+            throw true;
+        }
 
-		if (!xdoc.load_string((char*)bt.get())) {
-			//TODO: log sth
-			throw false;
-		}
+        if (!xdoc.load_string((char*)bt.get())) {
+            //TODO: log sth
+            throw false;
+        }
 
-		if (!m_BaseEntity->Read(xdoc.document_element())) {
-			AddLog(Hint, "Failed to read epx Entities: " << m_CurrentPatternFile);
-			throw false;
-		}
+        if (!m_BaseEntity->Read(xdoc.document_element())) {
+            AddLog(Hint, "Failed to read epx Entities: " << m_CurrentPatternFile);
+            throw false;
+        }
 
-		m_CurrentPatternFile = file;
-		AddLog(Hint, "Opened epx: " << m_CurrentPatternFile);
-		Refresh();
-		return true;
-	}
-	catch (...) {
-		Clear();
-		Refresh();
-		ErrorMessage("Failure during opening data!");
-		AddLog(Hint, "Failed to open epx: " << file);
-		return false;
-	}
+        m_CurrentPatternFile = file;
+        AddLog(Hint, "Opened epx: " << m_CurrentPatternFile);
+        Refresh();
+        return true;
+    }
+    catch (...) {
+        Clear();
+        Refresh();
+        ErrorMessage("Failure during opening data!");
+        AddLog(Hint, "Failed to open epx: " << file);
+        return false;
+    }
 }
 
 bool EntityEditorWindow::SaveData() {
-	if (!IsChanged())
-		return false;
+    if (!IsChanged())
+        return true;
 
-	try {
-		pugi::xml_document xdoc;
-		if (!m_BaseEntity->Write(xdoc.append_child("Entity"))) {
-			AddLog(Hint, "Failed to write epx Entities: " << m_CurrentPatternFile);
-			throw false;
-		}
+    try {
+        pugi::xml_document xdoc;
+        if (!m_BaseEntity->Write(xdoc.append_child("Entity"))) {
+            AddLog(Hint, "Failed to write epx Entities: " << m_CurrentPatternFile);
+            throw false;
+        }
 
-		std::stringstream ss;
-		xdoc.save(ss);
-		StarVFS::ByteTable bt;
-		bt.from_string(ss.str());
+        std::stringstream ss;
+        xdoc.save(ss);
+        StarVFS::ByteTable bt;
+        bt.from_string(ss.str());
 
-		auto fs = MainWindow::Get()->GetFilesystem();
-		if (!fs->SetFileData(m_CurrentPatternFile.c_str(), bt)) {
-			//todo: log sth
-			return false;
-		}
+        auto fs = MainWindow::Get()->GetFilesystem();
+        if (!fs->SetFileData(m_CurrentPatternFile.c_str(), bt)) {
+            //todo: log sth
+            return false;
+        }
 
-		SetModiffiedState(false);
-		AddLog(Hint, "Saved epx: " << m_CurrentPatternFile);
+        SetModiffiedState(false);
+        AddLog(Hint, "Saved epx: " << m_CurrentPatternFile);
 
-		return true;
-	}
-	catch (...) {
-		AddLog(Hint, "Failed to save epx: " << m_CurrentPatternFile);
-		return false;
-	}
+        return true;
+    }
+    catch (...) {
+        AddLog(Hint, "Failed to save epx: " << m_CurrentPatternFile);
+        return false;
+    }
 }
 
 //----------------------------------------------------------------------------------
