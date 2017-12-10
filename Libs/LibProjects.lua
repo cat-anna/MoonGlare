4b309013ac9306742b4f3a677eb1baa1056402a3
Pawel Grabas <pgrabas@gmail.com>
2017-12-10 12:51:05 +0100

    [master] svfs update

--- Libs/LibProjects.lua
+++ Libs/LibProjects.lua
@@ -1,7 +1,6 @@
 
 include "StarVFS/core/project.lua"
 include "StarVFS/svfs/project.lua"
-include "StarVFS/svfslua/project.lua"
 
 include "OrbitLogger/src/library.lua"
 include "libSpace/src/library.lua"
