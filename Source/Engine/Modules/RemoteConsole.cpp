9f69192d6507ae1757a8e666fa0140e6092583d0
Paweł Grabas <pgrabas@gmail.com>
2018-08-04 21:22:39 +0200

    [develop] changed method of storing remote console settings

--- Source/Engine/Modules/RemoteConsole.cpp
+++ Source/Engine/Modules/RemoteConsole.cpp
@@ -10,19 +10,18 @@
 #include <Foundation/MoonGlareInsider.h>
 #include <Core/Scripts/ScriptEngine.h>
 
+#include <Foundation/InterfaceMap.h>
+#include <Foundation/Settings.h>
+
 #define _WIN32_WINNT 0x0502
 #include <boost/asio.hpp>
-
-#include <RemoteConsole.x2c.h>
 
 namespace MoonGlare::Modules {
 
 using namespace Debug::InsiderApi;
 
 struct RemoteConsoleModule : public iModule {
-    RemoteConsoleModule(World *world) : iModule(world) {
-        m_Settings.ResetToDefault();
-    }
+    RemoteConsoleModule(World *world) : iModule(world) { }
 
     ~RemoteConsoleModule() {
         m_Running = false;
@@ -34,23 +33,24 @@ struct RemoteConsoleModule : public iModule {
     virtual std::string GetName() const { return "RemoteConsole"; };
 
     virtual void OnPostInit() {
-        if(m_Settings.m_Enabled)
-            m_Thread = std::thread(&RemoteConsoleModule::ThreadEntry, this);
-    }
+        auto stt = GetWorld()->GetInterface<Settings>();
+        if (stt) {
+            enabled = stt->GetBool("RemoteConsole.Enabled", enabled);
+            port = stt->GetInt("RemoteConsole.Port", port);
+        }
 
-    void LoadSettings(const pugi::xml_node node) override {
-        m_Settings.Read(node);
-    }
-    void SaveSettings(pugi::xml_node node) const override {
-        m_Settings.Write(node);
+        if (enabled) {
+            m_Thread = std::thread(&RemoteConsoleModule::ThreadEntry, this);
+        }
     }
 private:
     using udp = boost::asio::ip::udp;
 
-    x2c::Module::RemoteConsole::RemoteConsoleSettings_t m_Settings;
+    bool enabled = false;
+    uint16_t port = Debug::InsiderApi::Configuration::recon_Port;
 
     std::thread m_Thread;
-    volatile bool m_Running;
+    bool m_Running;
     boost::asio::io_service m_ioservice;
 
     void ThreadEntry() {
@@ -60,12 +60,11 @@ private:
         char buffer[Debug::InsiderApi::Configuration::MaxMessageSize];
         auto *header = reinterpret_cast<MessageHeader*>(buffer);
 
-        udp::socket sock(m_ioservice, udp::endpoint(udp::v4(), m_Settings.m_Port));
+        udp::socket sock(m_ioservice, udp::endpoint(udp::v4(), port));
 
         AddLog(Hint, "Remote console initialized");
         while (m_Running) {
             try {
-                //auto len = 1
                 if (sock.available() <= 0) {
                     std::this_thread::sleep_for(std::chrono::milliseconds(10));
                     continue;
@@ -78,8 +77,7 @@ private:
                     continue;
 
                 switch (header->MessageType) {
-                case MessageTypes::ExecuteCode:
-                {
+                case MessageTypes::ExecuteCode: {
                     AddLogf(Info, "Received lua command. Size: %d bytes. Data: %s ", header->PayloadSize, header->PayLoad);
                     MoonGlare::Core::GetScriptEngine()->ExecuteCode((char*)header->PayLoad, header->PayloadSize - 1, "RemoteConsole");
                     break;
